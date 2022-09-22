beada5f35b3819dd18cbef3079f3e94b3592ed01
goddemi <goddemi0124@gmail.com>
2022-09-22 22:26:21 +0900

    Add : node 기본 get, post요청과 응답

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
 const express = require("express");
 const app = express();
 
-app.listen();
+app.listen(8080, () => {
+  console.log("server open in 8080");
+});
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/pet", function (req, res) {
+  res.send("Hi nice to meet you");
+});
+
+app.get("/beauty", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/add", (req, res) => {
+  console.log(req.body);
+  res.send("전송완료");
+});
