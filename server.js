f72cd7662573fd406a8d93e8bc5ad217748316eb
goddemi <goddemi0124@gmail.com>
2022-09-29 18:49:25 +0900

    Add : 서버에서 받아오기

--- server.js
+++ server.js
@@ -11,12 +11,34 @@ MongoClient.connect(
 
     Database = client.db("todoapp"); //투두앱 이라는 폴더를 데이터베이스로 가져온다.
 
-    Database.collection("post").insertOne(
-      { name: "강철", age: 32 },
-      function (err, res) {
-        console.log("저장완료");
-      }
-    );
+    app.use(express.urlencoded({ extended: true }));
+
+    app.get("/pet", function (req, res) {
+      res.send("Hi nice to meet you");
+    });
+
+    app.get("/beauty", (req, res) => {
+      res.sendFile(__dirname + "/index.html");
+    });
+
+    app.post("/add", (req, res) => {
+      Database.collection("post").insertOne(
+        { name: req.body.title, detail: req.body.detail },
+        function (err, res) {
+          console.log("저장완료");
+        }
+      );
+
+      res.send("전송완료");
+    });
+
+    app.get("/list", function (req, res) {
+      Database.collection("post")
+        .find()
+        .toArray(function (err, res) {
+          console.log(res);
+        });
+    });
 
     // post라는 파일에 insert 한다. 그냥 형식을 외우면 된다. 저장해두고 필요할 때 꺼내 쓰면 되는 형식.
 
@@ -28,18 +50,3 @@ MongoClient.connect(
 
 //데이터 베이스 접속이 완료되면 서버를 만들어 주어라. 라는 의미.
 //에러 처리.
-
-app.use(express.urlencoded({ extended: true }));
-
-app.get("/pet", function (req, res) {
-  res.send("Hi nice to meet you");
-});
-
-app.get("/beauty", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
-
-app.post("/add", (req, res) => {
-  console.log(req.body);
-  res.send("전송완료");
-});
