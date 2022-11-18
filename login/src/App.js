f9559785e293a82df6d7d36eb8cdbfd16a77ee67
goddemi <goddemi0124@gmail.com>
2022-11-18 15:11:39 +0900

    login setting

---
+++ login/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <form>
+        <div>
+          <input />
+        </div>
+        <div>
+          <input />
+        </div>
+        <button>button</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
